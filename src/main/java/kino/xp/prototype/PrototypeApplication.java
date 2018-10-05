bd69f2a41a5b28d07b0ff9f4f8017186cb072b4d
Jonas800 <jonasolsenit@gmail.com>
2018-10-05 09:47:41 +0200

    First draft environment

---
+++ src/main/java/kino/xp/prototype/PrototypeApplication.java
@@ -0,0 +1,12 @@
+package kino.xp.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+}
