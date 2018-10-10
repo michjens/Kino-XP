42be959d9be31c38f2dc699b988d806e223c47ca
Jonas800 <jonasolsenit@gmail.com>
2018-10-10 10:33:33 +0200

    dbconn

---
+++ Kino-XP-program/src/main/java/kino/xp/prototype/dbConn.java
@@ -0,0 +1,42 @@
+/**********
+ * Denne fil er baseret på dbAccess fra Fronter
+ *********/
+
+package kino.xp.prototype;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConn {
+
+
+    static dbConn instance = new dbConn();
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://127.0.0.1:3306/pumpestationen?useSSL=false";
+    static Connection con;
+
+    /**
+     * we want to use JDBC protocol, mysql DBMS , the local host with
+     * the database ap
+     */
+
+    public Connection createConnection() {
+        con = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            return con = DriverManager.getConnection(DATABASE_URL, "root", "rootpw");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static dbConn getInstance() {
+        return instance;
+    }
+
+
+}
