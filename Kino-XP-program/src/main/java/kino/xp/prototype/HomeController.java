5eabe1300e2d8d614461412382277a77705ab929
MatthiasSkou <ms@klinikskou.dk>
2018-10-12 11:33:00 +0200

    k

--- Kino-XP-program/src/main/java/kino/xp/prototype/HomeController.java
+++ Kino-XP-program/src/main/java/kino/xp/prototype/HomeController.java
@@ -39,7 +39,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String forside(Model model) throws SQLException {
-        //ArrayList<Film> films = new ArrayList<>();
+        ArrayList<Film> films = new ArrayList<>();
         films = loadMovies();
         //Film film = new Film("https://dqj2xt0g8bn02.cloudfront.net/cdn/farfuture/EN4wI_6eHY1xCgm8CI78pAYp0k8f_EoTB4dhR9wsbwY/mtime:1521713019/files/styles/220_315_scale_crop/public/terrifier-2017-clown-horror-film-movie-poster.png?itok=smrTw0Zp", "name", 100, 15, "bob", "horror", 100);
         //Film film1 = new Film("https://www.henshaws.org.uk/wp-content/uploads/2017/01/film-016.jpg", "titel", 50, 20, "hej", "lol", 100);
