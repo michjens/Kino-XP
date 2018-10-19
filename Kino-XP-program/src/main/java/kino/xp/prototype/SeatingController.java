f6f91b0ba994593e29ac756556de8eba11089870
ortismal <33696981+ortismal@users.noreply.github.com>
2018-10-19 10:17:48 +0200

    Slettet JS og tilføjet til seating-HTML

--- Kino-XP-program/src/main/java/kino/xp/prototype/SeatingController.java
+++ Kino-XP-program/src/main/java/kino/xp/prototype/SeatingController.java
@@ -1,6 +1,7 @@
 package kino.xp.prototype;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class SeatingController {
 
     @GetMapping("/seating")
-    public String seating(){
-
+    public String seating(Model model){
+        model.addAttribute("test", "hey");
         return "seating";
     }
 
